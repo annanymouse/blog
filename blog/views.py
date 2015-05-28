8ea4a532ff25cbd778ba5841311283fa2f459059
annanymouse <annachull@gmail.com>
2015-05-28 08:23:37 +0000

    Got viewing single posts to work, as well as editing of posts and deletion of posts.

--- blog/views.py
+++ blog/views.py
@@ -4,6 +4,9 @@ from blog import app
 from .database import session
 from .models import Post
 
+import mistune
+from flask import request, redirect, url_for
+
 # @app.route("/")
 # def posts():
 #     posts = session.query(Post)
@@ -39,3 +42,66 @@ def posts(page=1, paginate_by=10):
         page=page,
         total_pages=total_pages
     )
+
+@app.route("/post/add", methods=["GET"])
+def add_post_get():
+    return render_template("add_post.html")
+
+@app.route("/post/add", methods=["POST"])
+def add_post_post():
+    post = Post(
+        title=request.form["title"],
+        content=mistune.markdown(request.form["content"]),
+    )
+    session.add(post)
+    session.commit()
+    return redirect(url_for("posts"))
+
+# /post/<id>
+# Allows you to view a single post
+# Should be accessed by clicking on the title of a post
+# Should use the render_post macro to display the post
+@app.route("/post/<int:id>")
+def post(id = None):
+    post = session.query(Post)
+    post = post.filter_by(id=id).first()
+    return render_template("post.html", post=post)
+
+# /post/<id>/edit
+# Allows you to edit a post
+# Should be accessed via a link in the metadata div
+# Should display a similar form to the add_post view
+# The form should be prepopulated with the existing post data
+@app.route("/post/<int:id>/edit", methods=["GET"])
+def edit_post(id = None):
+    post = session.query(Post)
+    post = post.filter_by(id=id).first()
+    return render_template("edit_post.html", post=post)
+
+@app.route("/post/<int:id>/edit", methods=["POST"])
+def edit_post_post(id=None):
+    post = session.query(Post)
+    post = post.filter_by(id=id).first()
+    post.title = request.form["title"]
+    post.content = mistune.markdown(request.form["content"])
+    session.commit()
+    return redirect(url_for("posts"))
+
+# /post/<id>/delete
+# Allows you to delete a post
+# Should be accessed via a link in the metadata div
+# Should display buttons asking you to confirm or cancel the deletion
+@app.route("/post/<int:id>/delete", methods=["GET"])
+def delete_post(id=None):
+    post = session.query(Post)
+    post = post.filter_by(id=id).first()
+    return render_template("delete_post.html", post=post)
+
+@app.route("/post/<int:id>/delete", methods=["POST"])
+def delete_post_post(id=None):
+    post = session.query(Post)
+    post = post.filter_by(id=id).first()
+    session.delete(post)
+    session.commit()
+    return redirect(url_for("posts"))
+
