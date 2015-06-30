ceee0ede2800b5399e5af688758203fc881a2c05
annanymouse <annachull@gmail.com>
2015-06-30 05:49:45 +0000

    starting to add user login/password functionality

--- blog/models.py
+++ blog/models.py
@@ -13,12 +13,12 @@ class Post(Base):
     content = Column(Text)
     datetime = Column(DateTime, default=datetime.datetime.now)
     
-# class User(Base, UserMixin):
-#     __tablename__ = "users"
+class User(Base, UserMixin):
+    __tablename__ = "users"
 
-#     id = Column(Integer, primary_key=True)
-#     name = Column(String(128))
-#     email = Column(String(128), unique=True)
-#     password = Column(String(128))
+    id = Column(Integer, primary_key=True)
+    name = Column(String(128))
+    email = Column(String(128), unique=True)
+    password = Column(String(128))
 
 Base.metadata.create_all(engine)
--- blog/views.py
+++ blog/views.py
@@ -4,6 +4,12 @@ from blog import app
 from .database import session
 from .models import Post
 
+
+from flask import flash
+from flask.ext.login import login_user
+from werkzeug.security import check_password_hash
+from .models import User
+
 import mistune
 from flask import request, redirect, url_for
 
@@ -104,3 +110,19 @@ def delete_post_post(id=None):
     session.delete(post)
     session.commit()
     return redirect(url_for("posts"))
+  
+@app.route("/login", methods=["GET"])
+def login_get():
+    return render_template("login.html")
+  
+@app.route("/login", methods=["POST"])
+def login_post():
+    email = request.form["email"]
+    password = request.form["password"]
+    user = session.query(User).filter_by(email=email).first()
+    if not user or not check_password_hash(user.password, password):
+        flash("Incorrect username or password", "danger")
+        return redirect(url_for("login_get"))
+
+    login_user(user)
+    return redirect(request.args.get('next') or url_for("posts"))
